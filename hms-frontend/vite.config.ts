c8cde05edfedddbd2d0db091703a0804746845f2
brianbrix <mokandubrian@gmail.com>
2025-10-16 04:59:10 +0300

    Deploy to server

--- hms-frontend/vite.config.ts
+++ hms-frontend/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+const isDocker = process.env.VITE_DOCKER === 'true'
 
 export default defineConfig({
   plugins: [react()],
+  base: isDocker ? '/hms/' : '/',
   define: {
     global: 'window'
   },
